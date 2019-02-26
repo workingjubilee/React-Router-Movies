a5883ae3f41ce7e327d298a093309e560205378c
workingjubilee <workingjubilee@gmail.com>
2019-02-26 14:00:33 -0800

    Finish Home navigation.

---
+++ client/src/Movies/SavedList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class SavedList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="saved-list">
+        <h3>Saved Movies:</h3>
+        {this.props.list.map(movie => (
+          <span className="saved-movie">{movie.title}</span>
+        ))}
+        <Link to="/" className="home-button">Home</Link>
+      </div>
+    );
+  }
+}
