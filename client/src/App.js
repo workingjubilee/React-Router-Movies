f9e1e6fe397f6c7ffce55d0e1158fe0e3b873424
workingjubilee <workingjubilee@gmail.com>
2019-02-25 16:04:00 -0800

    Part 1 complete.

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path="/" component={MovieList}/>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:movieID" component={Movie} />
       </div>
     );
   }
